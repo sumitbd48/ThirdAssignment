153d9ea465c857df38c63bda314c0ceb4b9c3219
sumitbd48 <sumit.buddhacharya48@gmail.com>
2019-11-20 22:00:27 +0545

    added webview to view any website from the internet

--- app/src/main/java/com/example/thirdassignment/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/thirdassignment/ui/notifications/NotificationsFragment.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -16,20 +19,18 @@ import com.example.thirdassignment.R;
 
 public class NotificationsFragment extends Fragment {
 
-    private NotificationsViewModel notificationsViewModel;
+    private WebView webView;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        notificationsViewModel =
-                ViewModelProviders.of(this).get(NotificationsViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_notifications, container, false);
-        final TextView textView = root.findViewById(R.id.text_notifications);
-        notificationsViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        View root = inflater.inflate(R.layout.fragment_notifications,container,false);
+
+        webView = root.findViewById(R.id.webView);
+
+        webView.loadUrl("https://softwarica.edu.np/");
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+
         return root;
     }
 }
